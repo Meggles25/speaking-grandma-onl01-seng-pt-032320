7191e8782d82490836b4fd3ff4d74b52e701ef5d
Megan Gomes <megan.gomes25@gmail.com>
2020-03-30 01:34:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,2 +1,10 @@
 def speak_to_grandma(phase)
-  if phase == 
+  if phase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase.upcase != phrase
+    return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
+  end
+  
+end  
